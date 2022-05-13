461d5e9e80feb379b821e0f831facadcd41a8a86
afrotonder <o.rosado18@gmail.com>
2022-05-13 17:02:46 -0400

    Move important stuff from skinnerbox alpha invaders to this wagmiballz clone.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
-
+import { initializeApp } from "firebase/app";
+import {firebaseConfig} from '../../environment.prod';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor() {
+    const app = initializeApp(firebaseConfig);
+
+  }
 }
